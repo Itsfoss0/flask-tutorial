8efcc17a4edc156b1be6adf487be4c78f771de59
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-20 10:35:38 +0300

    Added app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, flash
-
+import requests 
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/index",methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-    
+    return render_template("index.html")
+
+
+@app.route("/greetings", methods=['GET', 'POST'])
+def greet():
+    render_template("index.html", message=requests.form.get("message"))  
 if __name__ == "__main__":
     app.run(debug=False)
